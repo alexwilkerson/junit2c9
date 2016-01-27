584ab14c29c009bca183c2d24f23530846e117ca
Alex Wilkerson <alex@uncorrected.com>
2016-01-26 22:16:34 -0600

    updated classpath. added source

--- junit2c9.sh
+++ junit2c9.sh
@@ -11,8 +11,8 @@ echo "junit2c9 linking JUnit to Java classpath..."
 
 if [[ -f $JUNIT && -f $HAMCREST && -f $BASH_PROFILE ]]; then
     echo -e "\n# This section appended with junit2c9" >> $BASH_PROFILE 
-    echo "set JUNIT_HOME=/usr/share/java" >> $BASH_PROFILE
-    echo -e "export CLASSPATH=\$CLASSPATH:\$JUNIT_HOME/junit4.jar\n" >> $BASH_PROFILE
+    echo -e "export CLASSPATH=\$CLASSPATH:${JUNIT}:${HAMCREST}:.\n" >> $BASH_PROFILE
+    source $BASH_PROFILE
     exit 0
 else
     echo "One of the required files does not exist. Exiting..."
