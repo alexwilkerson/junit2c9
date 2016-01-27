4781cd93e7fd192536d1c8a8bebf7939e2822d13
Alex Wilkerson <alex@uncorrected.com>
2016-01-26 22:42:06 -0600

    finished?

--- junit2c9.sh
+++ junit2c9.sh
@@ -11,11 +11,12 @@ echo "junit2c9 linking JUnit to Java classpath..."
 
 if [[ -f $JUNIT && -f $HAMCREST && -f $BASH_PROFILE ]]; then
     echo -e "\n# This section appended with junit2c9" >> $BASH_PROFILE 
-    echo -e "export CLASSPATH=\$CLASSPATH:${JUNIT}:${HAMCREST}:." >> $BASH_PROFILE
+    echo -e "export CLASSPATH=${JUNIT}:${HAMCREST}:." >> $BASH_PROFILE
+    echo -e "\n# JUnit alias\nalias javajunit=\"java org.junit.runner.JUnitCore \""
     source $BASH_PROFILE
-    echo "Success!"
-    exit 0
 else
     echo "One of the required files does not exist. Exiting..."
     exit 1
 fi
+
+exit 0
